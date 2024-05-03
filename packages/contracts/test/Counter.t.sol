2088d9bcebcf1c58eec994dd2918ffd44de918e2
Vedant Chainani <vedantchainani1084@gmail.com>
2024-05-03 17:06:43 +0530

    chore: add solidity foundry template

--- packages/contracts/test/Counter.t.sol
+++ packages/contracts/test/Counter.t.sol
@@ -1,24 +1,42 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {Test, console2 as console, Vm} from "forge-std/Test.sol";
+import {Counter} from "src/Counter.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+import {SetUp} from "test/base/SetUp.t.sol";
 
-    function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+contract CounterTest is Test, SetUp {
+    Vm.Wallet public alice;
+    Vm.Wallet public bob;
+
+    function setUp() public virtual override {
+        super.setUp();
+        alice = vm.createWallet("alice");
+        bob = vm.createWallet("bob");
     }
 
     function test_Increment() public {
+        vm.startBroadcast(owner.addr);
+
+        vm.expectEmit(true, true, true, true);
+        emit NumberIncremented(owner.addr);
         counter.increment();
+
         assertEq(counter.number(), 1);
+
+        vm.stopBroadcast();
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function test_SetNumber() public {
+        vm.startBroadcast(owner.addr);
+
+        vm.expectEmit(true, true, true, true);
+        emit NumberSet(owner.addr, 1);
+        counter.setNumber(1);
+
+        assertEq(counter.number(), 1);
+
+        vm.stopBroadcast();
     }
 }
