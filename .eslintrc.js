2c18be1b972e8d482a696c4b58faeba976438b58
Vedant Chainani <vedantchainani1084@gmail.com>
2024-05-11 15:56:36 +0530

    chore: add global eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,8 @@
-const { resolve } = require('node:path');
-
-const project = resolve(process.cwd(), 'tsconfig.json');
-
-/** @type {import("eslint").Linter.Config} */
 module.exports = {
   ignorePatterns: ['apps/**', 'packages/**'],
-  extends: ['eslint:recommended', 'prettier', 'eslint-config-turbo'],
-  plugins: ['only-warn'],
+  extends: ['./packages/eslint-config/library.js'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
   },
-  globals: {
-    React: true,
-    JSX: true,
-  },
-  env: {
-    node: true,
-  },
-  settings: {
-    'import/resolver': {
-      typescript: {
-        project,
-      },
-    },
-  },
-  ignorePatterns: [
-    // Ignore dotfiles
-    '.*.js',
-    'node_modules/',
-    'dist/',
-  ],
-  overrides: [
-    {
-      files: ['*.js?(x)', '*.ts?(x)'],
-    },
-  ],
 };
